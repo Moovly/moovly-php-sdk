64511d89a37f85dd7fae514162ab0796e3491c50
Niels <niels.vermaut@moovly.com>
2018-06-21 11:04:36 +0200

    Update NotFoundException.php

--- src/Exception/NotFoundException.php
+++ src/Exception/NotFoundException.php
@@ -13,10 +13,10 @@ namespace Moovly\SDK\Exception;
 class NotFoundException extends MoovlyException
 {
     const CODE = 400;
-    const MESSAGE = 'The API call you made was against a non-existant endpoint or couldn\'t find the resource.';
+    const MESSAGE = 'The API call you made was against a non-existant endpoint or couldn\'t find the resource. Additional message given: %s';
 
-    public function __construct(string $reason)
+    public function __construct(?string $reason)
     {
-        parent::__construct(sprintf(self::MESSAGE, $reason), self::CODE);
+        parent::__construct(sprintf(self::MESSAGE, $reason ?? 'none'), self::CODE);
     }
 }
